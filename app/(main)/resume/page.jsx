7ab115686f8de072b6f6a4acf642a6e5cd420010
Abhinav <abhinav312003@gmail.com>
2025-07-11 15:08:13 +0530

    fixed the header

--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -1,6 +1,9 @@
 import { getResume } from "@/actions/resume"
 import ResumeBuilder from "./_components/resume-builder";
 
+// this is a server side static page but i want to make it 
+// dynamic so that it can fetch the latest resume content
+// here we are using this to force the page to be dynamic
 export const dynamic = 'force-dynamic';
 
 async function ResumePage() {
--- components/header.jsx
+++ components/header.jsx
@@ -27,6 +27,7 @@ import {
   StarsIcon,
 } from "lucide-react";
 import { checkUser } from "@/lib/checkUser";
+import Image from "next/image";
 
 const Header = async () => {
   await checkUser();
@@ -34,9 +35,10 @@ const Header = async () => {
     <header className="fixed top-0 w-full border-b bg-background opacity-80  backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between ">
         <Link href="/">
-          <h1 className="font-poppins font-semibold custom-gradient text-[30px] ">
+          <h1 className="max-sm:text-[20px] font-poppins font-semibold custom-gradient text-[30px] ">
             SkillMate AI
           </h1>
+         
         </Link>
 
         <div className="flex items-center space-x-4">
